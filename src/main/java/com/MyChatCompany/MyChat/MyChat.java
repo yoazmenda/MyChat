953d722893d58c40cb54e3e8efb85316a4490cd4
yoaz menda <yoaz@morphisec.com>
2016-07-29 02:14:24 +0300

    Migrated to gradle

--- src/main/java/com/MyChatCompany/MyChat/MyChat.java
+++ src/main/java/com/MyChatCompany/MyChat/MyChat.java
@@ -1,8 +1,12 @@
+package com.MyChatCompany.MyChat;
+
 public class MyChat{
 	
 	
 	public static void main(String[] args){
-		System.out.println("Hello from server:");
+
+		System.out.println("Hello from server: what");
+
 	}
 	
 }
