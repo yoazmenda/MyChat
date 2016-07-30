ea9cda724219e73b35ede1cb79b18d69962e8e73
yoaz menda <yoaz@morphisec.com>
2016-07-30 17:48:31 +0300

    Added Junit support

--- src/test/java/BeanFactoryTests.java
+++ src/test/java/BeanFactoryTests.java
@@ -2,14 +2,18 @@ import com.company.BeanFactory;
 import com.company.annotations.Component;
 import com.company.annotations.Inject;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 import static org.junit.Assert.assertEquals;
 
 @Component
+@RunWith(JUnit4.class)
 public class BeanFactoryTests {
 
     @Test
-    public void testRandomGeneratl(){
+    public void test1(){
+        System.out.println("In test");
         BeanFactory beanFactory = new BeanFactory("com.company");
         int beanCount = beanFactory .getBeanCount();
         assertEquals(2, beanCount);
